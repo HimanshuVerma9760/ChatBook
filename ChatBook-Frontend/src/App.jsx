cd0a8a57dc4d9abe1f6b4307a50d7792f57a324d
Himanshu Verma <humnavaverma@gmail.com>
2024-12-26 14:53:16 +0530

    fixed issues

--- ChatBook-Frontend/src/App.jsx
+++ ChatBook-Frontend/src/App.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import Chat from "./Components/Chat";
-import Timer from "./Components/Timer";
 
 function App() {
   return (
     <div className="App">
       <h1>ChatBook</h1>
       <Chat />
-      <Timer />
     </div>
   );
 }
